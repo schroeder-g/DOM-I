3d8111ccae0c8556e7e73d5c2e5eccd53f2fa2fd
Alex <schroedergoncalves@gmail.com>
2020-07-06 17:31:54 -0400

    completed main project

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ const siteContent = {
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
+    "address" : "123 Way 456 Street <br> Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
@@ -38,5 +38,81 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+// let logo = document.getElementById("logo-img");
+// logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// Create selectors by using any of the DOM element's methods
+const logo = document.querySelector('#logo-img')
+//Note that IDs have been used on all images. Use the IDs to update src path content
+logo.setAttribute('src', 'img/logo.png')
+console.log(logo)
+
+const callToAction = document.getElementById('cta-img')
+callToAction.setAttribute('src', 'img/header-img.png')
+console.log(callToAction)
+
+const accentImg = document.getElementById('middle-img')
+accentImg.setAttribute('src', 'img/mid-page-accent.jpg')
+console.log(accentImg)
+
+const navItems = document.querySelectorAll('a')
+navItems[0].innerHTML = "Services"
+navItems[1].innerHTML = "Product"
+navItems[2].innerHTML = "Vision"
+navItems[3].innerHTML = "Features"
+navItems[4].innerHTML = "About"
+navItems[5].innerHTML = "Contact"
+console.log(navItems)
+
+let title = document.querySelector('.cta-text h1')
+title.innerHTML = 'DOM <br> Is <br> Awesome'
+
+let ctaBtn = document.querySelector('.cta-text button')
+ctaBtn.innerHTML = siteContent.cta.button
+
+// main content: top- / bottom-content
+let firstH4 = document.querySelector('.top-content .text-content:nth-of-type(1) h4')
+firstH4.innerHTML = siteContent["main-content"]["features-h4"]
+
+let firstContent = document.querySelector('.top-content .text-content:nth-of-type(1) p')
+firstContent.innerHTML = siteContent["main-content"]["features-content"]
+
+let secondH4 = document.querySelector('.top-content .text-content:nth-of-type(2) h4')
+secondH4.innerHTML = siteContent["main-content"]["about-h4"]
+
+let secondContent = document.querySelector('.top-content .text-content:nth-of-type(2) p')
+secondContent.innerHTML = siteContent["main-content"]["about-content"]
+
+let thirdH4 = document.querySelector('.bottom-content .text-content:nth-of-type(1) h4')
+thirdH4.innerHTML = siteContent["main-content"]["services-h4"]
+
+let thirdContent = document.querySelector('.bottom-content .text-content:nth-of-type(1) p')
+thirdContent.innerHTML = siteContent['main-content']['services-content']
+
+let fourthH4 = document.querySelector('.bottom-content .text-content:nth-of-type(2) h4')
+fourthH4.innerHTML = siteContent["main-content"]["product-h4"]
+
+let fourthContent = document.querySelector('.bottom-content .text-content:nth-of-type(2) p')
+fourthContent.innerHTML = siteContent['main-content']['product-content']
+
+let fifthH4 = document.querySelector('.bottom-content .text-content:nth-of-type(3) h4')
+fifthH4.innerHTML = siteContent['main-content']['vision-h4']
+
+let fifthContent = document.querySelector('.bottom-content .text-content:nth-of-type(3) p')
+fifthContent.innerHTML = siteContent['main-content']['vision-content']
+
+//contact page
+
+let contactTitle = document.querySelector(".contact h4")
+contactTitle.innerHTML = siteContent.contact["contact-h4"]
+
+let address = document.querySelector(".contact p:nth-of-type(1)")
+address.innerHTML = siteContent.contact.address
+console.log(address)
+
+let phone = document.querySelector(".contact p:nth-of-type(2)")
+phone.innerHTML = siteContent.contact.phone
+
+let email = document.querySelector(".contact p:nth-of-type(3)")
+email.innerHTML = siteContent.contact.email
